facff70e2a4ccda86d88aa5fa9eb20ec810a13ea
wjdghks3431 <wjdghks3431@naver.com>
2023-03-04 14:55:20 +0900

    돔을 이용하여 HTML객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,19 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+//DOMException(문서 객체 모델 :인터페이스)
+//태그랑 아이디 같게 하면 안됨 #-- id로 부여된 이름을 가져온다
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+    const req ={
+        id : id.value,
+        password : password.value,
+    };
+    console.log(req);
+
+}
+
